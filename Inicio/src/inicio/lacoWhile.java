24db7dea9feab4d129186311253d4291a3133ccf
elder <elder@game>
2021-02-10 20:50:57 -0300

    teste

--- Inicio/src/inicio/lacoWhile.java
+++ Inicio/src/inicio/lacoWhile.java
@@ -1,5 +1,17 @@
 package inicio;
+import java.util.Scanner;
 
 public class lacoWhile {
-
+	public static void main(String[] args) {
+					
+		Scanner ler = new Scanner(System.in);
+		int numero = ler.nextInt();
+		int soma = 0; 
+		
+		while (numero != 0) {
+			soma = soma + numero;
+			numero = ler.nextInt();					
+		}
+		System.out.println(soma);
+	}
 }
