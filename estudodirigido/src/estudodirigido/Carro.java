8519460ff9960b2816fe4c386bf7ea8c2f920c5f
Elder Matheus Araújo da Incarnação <73678239+eldermatheus@users.noreply.github.com>
2021-02-12 17:23:32 -0300

    teste

--- estudodirigido/src/estudodirigido/Carro.java
+++ estudodirigido/src/estudodirigido/Carro.java
@@ -1,7 +1,27 @@
 package estudodirigido;
 
-public class Carro {
-	String tipoCarro;
-	int portaMalas;
+public class Carro extends Veiculo {
+	private String tipoCarro;
+	private int portaMalas;
 	
+	public Carro(String placa, int anoFabricacao, String anoModelo, String cor, String chassi,int quilometragem, double valorDiaria) {
+		super(placa, anoFabricacao, anoModelo, cor, chassi, quilometragem, valorDiaria);
+	}
+	
+	public String getTipoCarro() {
+		return tipoCarro;
+	}
+	
+	public void setTipoCarro(String tipoCarro) {
+		this.tipoCarro = tipoCarro;
+	}	
+	
+	public int getPortaMalas() {
+		return portaMalas;
+	}
+	
+	public void setPortaMalas(int portaMalas) {
+		this.portaMalas = portaMalas;
+	}
+			
 }
