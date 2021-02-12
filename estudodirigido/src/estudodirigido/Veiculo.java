5aede885d2ec1856cd5d2277e9f9c571f3b895cf
Elder Matheus Araújo da Incarnação <73678239+eldermatheus@users.noreply.github.com>
2021-02-12 14:12:08 -0300

    teste

---
+++ estudodirigido/src/estudodirigido/Veiculo.java
@@ -0,0 +1,12 @@
+package estudodirigido;
+
+public class Veiculo {
+		String placa;
+		int anoFabricacao;
+		String anoModelo;
+		String cor;
+		String chassi;
+		int quilometragem;
+		double valorDiaria;
+		
+}
